bbb786eb3c0a99e361898839b36de330ceedd591
Vishal Girhepunje <girhepunjevishal@gmail.com>
2022-09-13 13:42:36 +0530

    string name change

--- prime_detection.js
+++ prime_detection.js
@@ -2,11 +2,11 @@ let num = 13;
 let factors = 0;
 for(let i=1;i<=num;i++){
    if(num%i==0){
-     factors+=2
+     factors++;
    }
 }
 if(factors===2){
-  console.log(num,"is a prime");
+  console.log(num,"is prime");
 }else{
-  console.log(num,"is not a prime");
+  console.log(num,"is not prime");
 }
